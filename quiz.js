f39863509eb165d8cbbf887d271e6a73c2d29231
Jufebrown <jufebrown@gmail.com>
2016-12-11 20:33:22 -0600

    changed className to classList.add

--- quiz.js
+++ quiz.js
@@ -14,32 +14,32 @@ function putCardsInRows() {
 
 function newCard(inventoryObj) {
   var newCard = document.createElement("article");
-  newCard.className("col-md-4");
+  newCard.classList.add("col-md-4");
   var makeP = document.createElement("p");
-  makeP.className("make");
+  makeP.classList.add("make");
   makeP.innerHTML = inventoryObj.make;
   newCard.appendChild(makeP);
   var modelP = document.createElement("p");
-  modelP.className("model");
+  modelP.classList.add("model");
   modelP.innerHTML = inventoryObj.model;
   newCard.appendChild(modelP);
   var yearP = document.createElement("p");
-  yearP.className("year");
+  yearP.classList.add("year");
   yearP.innerHTML = inventoryObj.year;
   newCard.appendChild(yearP);
   var priceP = document.createElement("p");
-  priceP.className("price");
+  priceP.classList.add("price");
   priceP.innerHTML = inventoryObj.price;
   newCard.appendChild(priceP);
   var descriptionP = document.createElement("p");
-  descriptionP.className("description");
+  descriptionP.classList.add("description");
   descriptionP.innerHTML = inventoryObj.description;
   newCard.appendChild(descriptionP);
 }
 
 function newRow() {
   var newRowDiv = document.createElement("div");
-  newRowDiv.className(`addedRow${rowCounter} row`);
+  newRowDiv.classList.add(`addedRow${rowCounter} row`);
   rowArray.push(newRowDiv)
 }
 
